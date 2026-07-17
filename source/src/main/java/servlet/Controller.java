72274f2fe6ddb009f8de01f7523cb58ff44b1201
shimizu-shimizu-shimizu <hinata.shimizu@ysl.co.jp>
2026-07-17 15:21:10 +0900

    ドロワーメニュー整備

--- source/src/main/java/servlet/Controller.java
+++ source/src/main/java/servlet/Controller.java
@@ -9,89 +9,119 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	        throws ServletException, IOException {
-	    request.setCharacterEncoding("UTF-8");
-	    
-	    //  ページidを取得し、画面遷移
-	    //  仮だしドロワーメニュー用だよ
-	    String pageId = request.getParameter("page_id");
-	    String page = "/WEB-INF/jsp/login.jsp";
+		request.setCharacterEncoding("UTF-8");
 
-	    if (pageId == null || pageId.equals("toHome")) {
-	        page = "/WEB-INF/jsp/home.jsp";
+		// ドロワーメニューなどから受け取る画面ID
+		String pageId = request.getParameter("page_id");
 
-	    } else if (pageId.equals("toProjectList")) {
-	        page = "/WEB-INF/jsp/project/project_list.jsp";
+		// 指定なしの場合はホーム
+		String page = "/WEB-INF/jsp/home.jsp";
 
-	    } else if (pageId.equals("toTaskList")) {
-	        page = "/WEB-INF/jsp/task/task_list.jsp";
+		if (pageId == null || pageId.equals("toHome")) {
+			page = "/WEB-INF/jsp/home.jsp";
 
-	    } else if (pageId.equals("toMonth")) {
-	        page = "/WEB-INF/jsp/month.jsp";
+		} else if (pageId.equals("projectList")) {
+			page = "/WEB-INF/jsp/project/project_list.jsp";
 
-	    } else if (pageId.equals("toMypage")) {
-	        page = "/WEB-INF/jsp/mypage.jsp";
+		} else if (pageId.equals("projectDetail")) {
+			page = "/WEB-INF/jsp/project/project_detail.jsp";
 
-	    } else if(pageId.equals("toMemberList")) {
-	        page = "/WEB-INF/jsp/member/member_list.jsp";
-	        
-	    } else if (pageId.equals("toLogout")) {
-	        // 後でログアウト処理を入れる
-	        page = "/WEB-INF/jsp/login.jsp";
-	    }
+		} else if (pageId.equals("projectRegist")) {
+			page = "/WEB-INF/jsp/project/project_regist.jsp";
 
-	    RequestDispatcher dispatcher = request.getRequestDispatcher(page);
-	    dispatcher.forward(request, response);
+		} else if (pageId.equals("projectEdit")) {
+			page = "/WEB-INF/jsp/project/project_edit.jsp";
+
+			// タスク
+		} else if (pageId.equals("taskList")) {
+			page = "/WEB-INF/jsp/task/task_list.jsp";
+
+		} else if (pageId.equals("taskDetail")) {
+			page = "/WEB-INF/jsp/task/task_detail.jsp";
+
+		} else if (pageId.equals("taskRegist")) {
+			page = "/WEB-INF/jsp/task/task_regist.jsp";
+
+		} else if (pageId.equals("taskEdit")) {
+			page = "/WEB-INF/jsp/task/task_edit.jsp";
+
+			// 工数
+		} else if (pageId.equals("workRegist")) {
+			page = "/WEB-INF/jsp/work_regist.jsp";
+
+			// 月次・マイページ
+		} else if (pageId.equals("month")) {
+			page = "/WEB-INF/jsp/month.jsp";
+
+		} else if (pageId.equals("mypage")) {
+			page = "/WEB-INF/jsp/mypage.jsp";
+
+			// メンバー管理
+		} else if (pageId.equals("memberList")) {
+			page = "/WEB-INF/jsp/member/member_list.jsp";
+
+		} else if (pageId.equals("memberRegist")) {
+			page = "/WEB-INF/jsp/member/member_regist.jsp";
+
+		} else if (pageId.equals("memberEdit")) {
+			page = "/WEB-INF/jsp/member/member_edit.jsp";
+
+			// ログイン
+		} else if (pageId.equals("login")) {
+			page = "/WEB-INF/jsp/login.jsp";
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-//		
-//		String page = null;
-//		
-//		//ページIDを取得
-//		String pageId = request.getParameter("page_id");
-//		//ボタンの詳細を取得
-//		String buttonId = request.getParameter("button_id");
-//		
-//		//何が入っているか確認
-//		System.out.println("ページ："+pageId+" ボタン："+buttonId);
-//		
-//		//ここから区分けして、何の処理をさせるかを指定
-//		if(pageId.equals("none") && buttonId.equals("ログアウト")) {
-//			//ログアウトボタンが押されたら
-//			HttpSession session = request.getSession();
-//			session.invalidate();
-//			page = "/WEB-INF/jsp/login.jsp";		
-//		}else if(pageId.equals("L001") && buttonId.equals("ログイン")) {
-//			//最初のページからログインボタンが押されたら
-//			UserAction action = new UserAction(request);
-//			page = action.login();
-//		}else if(pageId.equals("M001") && buttonId.equals("登録")) {
-//			//メニューから登録ボタンが押されたら
-//			UserAction action = new UserAction(request);
-//			page = action.regist();
-//		}else if(pageId.equals("M002") && buttonId.equals("削除")) {
-//			//メニューから削除ボタンが押されたら
-//			UserAction action = new UserAction(request);
-//			page = action.delete();
-//		}
-//		if(buttonId.equals("ホームへ")) {
-//			page = "/WEB-INF/jsp/home.jsp";	
-//		}else if(buttonId.equals("案件一覧へ")) {
-//			page = "/WEB-INF/jsp/project_list.jsp";	
-//		}
-//		
-//		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
-//		dispatcher.forward(request, response);
+		//		
+		//		String page = null;
+		//		
+		//		//ページIDを取得
+		//		String pageId = request.getParameter("page_id");
+		//		//ボタンの詳細を取得
+		//		String buttonId = request.getParameter("button_id");
+		//		
+		//		//何が入っているか確認
+		//		System.out.println("ページ："+pageId+" ボタン："+buttonId);
+		//		
+		//		//ここから区分けして、何の処理をさせるかを指定
+		//		if(pageId.equals("none") && buttonId.equals("ログアウト")) {
+		//			//ログアウトボタンが押されたら
+		//			HttpSession session = request.getSession();
+		//			session.invalidate();
+		//			page = "/WEB-INF/jsp/login.jsp";		
+		//		}else if(pageId.equals("L001") && buttonId.equals("ログイン")) {
+		//			//最初のページからログインボタンが押されたら
+		//			UserAction action = new UserAction(request);
+		//			page = action.login();
+		//		}else if(pageId.equals("M001") && buttonId.equals("登録")) {
+		//			//メニューから登録ボタンが押されたら
+		//			UserAction action = new UserAction(request);
+		//			page = action.regist();
+		//		}else if(pageId.equals("M002") && buttonId.equals("削除")) {
+		//			//メニューから削除ボタンが押されたら
+		//			UserAction action = new UserAction(request);
+		//			page = action.delete();
+		//		}
+		//		if(buttonId.equals("ホームへ")) {
+		//			page = "/WEB-INF/jsp/home.jsp";	
+		//		}else if(buttonId.equals("案件一覧へ")) {
+		//			page = "/WEB-INF/jsp/project_list.jsp";	
+		//		}
+		//		
+		//		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		//		dispatcher.forward(request, response);
 	}
 
 }
--- source/src/main/webapp/WEB-INF/jsp/common/header.jsp
+++ source/src/main/webapp/WEB-INF/jsp/common/header.jsp
@@ -10,12 +10,49 @@
 <nav id="menu" class="menu">
   <button id="closeButton" class="close-button" type="button">×</button>
 
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toHome">ホーム</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toProjectList">案件</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toTaskList">タスク</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toMonth">月次集計</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toMypage">マイページ</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toMemberList">メンバー</a>
-  <a href="${pageContext.request.contextPath}/Controller?page_id=toLogout">ログアウト</a>
+  <p class="menu-title">メインメニュー</p>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=home">ホーム</a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=projectList">案件一覧</a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=taskList">タスク一覧</a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=month">月次集計</a>
+    <a href="${pageContext.request.contextPath}/Controller?page_id=memberList">
+    メンバー一覧
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=mypage">マイページ</a>
+  <%-- 今だけ --%>
+  <div class="now-menu">  <a href="${pageContext.request.contextPath}/Controller?page_id=projectDetail">
+    案件詳細
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=projectRegist">
+    案件登録
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=projectEdit">
+    案件編集
+  </a>
+
+  <a href="${pageContext.request.contextPath}/Controller?page_id=taskDetail">
+    タスク詳細
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=taskRegist">
+    タスク登録
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=taskEdit">
+    タスク編集
+  </a>
+
+  <a href="${pageContext.request.contextPath}/Controller?page_id=workRegist">
+    工数登録
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=memberRegist">
+    メンバー登録
+  </a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=memberEdit">
+    メンバー編集
+  </a>
+
+  <a href="${pageContext.request.contextPath}/Controller?page_id=login">ログイン画面</a>
+  <a href="${pageContext.request.contextPath}/Controller?page_id=logout">ログアウト</a></div>
+
+
 </nav>
     
