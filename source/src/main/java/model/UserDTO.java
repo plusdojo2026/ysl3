4f47973f67b65c56afd36ea0ca27a2f1d2833abd
rin-ogasawara-rin <rin.ogasawara@ysl.co.jp>
2026-07-17 18:03:12 +0900

    UserDTO

--- source/src/main/java/model/UserDTO.java
+++ source/src/main/java/model/UserDTO.java
@@ -21,4 +21,64 @@ public UserDTO(int id, String loginId, String password, String mail, int role, i
 	
 }
 
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getLoginId() {
+	return loginId;
+}
+
+public void setLoginId(String loginId) {
+	this.loginId = loginId;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getMail() {
+	return mail;
+}
+
+public void setMail(String mail) {
+	this.mail = mail;
+}
+
+public int getRole() {
+	return role;
+}
+
+public void setRole(int role) {
+	this.role = role;
+}
+
+public int getSol() {
+	return sol;
+}
+
+public void setSol(int sol) {
+	this.sol = sol;
+}
+
+public UserDTO() {	
+	super();
+	this.id = 0;
+	this.loginId = "";
+	this.password = "";
+	this.mail = "";
+	this.role = 0;
+	this.sol = 0;
+
+
+}
+
 }
